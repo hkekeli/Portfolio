20b7f863c5af6a109e999d1a284ba24d2aea805a
Kekeli Sackey <hkekeli@users.noreply.github.com>
2017-03-25 20:50:53 -0400

    Postcss - clip-path should now work in firefox

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+    module: {
+        loaders: [
+            {
+                test: /\.css$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'style-loader',
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            sourceMap: true,
+                            importLoaders: 1,
+                        }
+                    },
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            sourceMap: 'inline',
+                        }
+                    }
+                ]
+            }
+        ]
+    }
+}
