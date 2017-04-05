a83706780a9f01481b827240e29420f3ad7b4ab5
Kekeli Sackey <hkekeli@users.noreply.github.com>
2017-04-04 22:24:47 -0400

    fix merge conflict

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const bird             = document.querySelector('.shape'),
       dot              = document.querySelector('.dot'),
       lowerBeak        = document.querySelector('.beak--lower'),
@@ -17,32 +16,4 @@ bird.onmouseenter = function(){
 }
 bird.onmouseleave = function(){
     toggleMovement();
-=======
-let bird = document.querySelector('.shape'),
-    dot  = document.querySelector('.dot'),
-    lowerBeak = document.querySelector('.beak--lower'),
-    headWrapperChild = document.querySelector('.head-wrapper-child'),
-    birdChildren = bird.children;
-
-bird.onmouseenter = function() {
-    bird.classList.add('pause-animation');
-    dot.classList.add('pause-animation');
-    lowerBeak.classList.add('pause-animation');
-    headWrapperChild.classList.add('pause-animation');
-    
-    for(var i = 0; i < birdChildren.length; i++) {
-        birdChildren[i].classList.add('pause-animation');
-    }
-}
-
-bird.onmouseleave = function() {
-    bird.classList.remove('pause-animation');
-    dot.classList.remove('pause-animation');
-    lowerBeak.classList.remove('pause-animation');
-    headWrapperChild.classList.remove('pause-animation');
-    
-    for(var i = 0; i < birdChildren.length; i++) {
-       birdChildren[i].classList.remove('pause-animation');
-    }
->>>>>>> pause bird on hover
 }
